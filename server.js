0a87e2849dfb2ed5665df1608f21bcc6287499e0
Nitik <nitik@cyntexa.com>
2021-09-07 17:57:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,21 +9,7 @@ var password="";
 // Serve static files
 app.use(express.static(__dirname + '/public'));
 app.post("/credential", async function(req,res){
-    console.log("req=="+req);
-    var passText={
-        "email":req.body.username,
-        "pass":req.body.password
-    };
-    console.log("passText.email=="+passText.email);
-    console.log("passText.pass=="+passText.pass);
-    if(id==passText.email && password==passText.pass)
-    {
-        console.log("Success");
-    }
-    else
-    {
-       console.log("Failed"); 
-    }
+    console.log("hello")
 });
 // Serve your app
 console.log('Served: http://localhost:' + port);
