00cccb0b4e2fcba12e3d6a1548cfd884b7ce99de
Khushboo <khushboo@cyntexa.com>
2021-09-13 19:02:57 +0530

    commit

--- server.js
+++ server.js
@@ -62,8 +62,9 @@ request(options, function (error, response) {
 
 
 app.post("/DEListFetch", async function (req, res) {
+    console.log('12 '+JSON.stringify(req));
+    console.log('hello 2 '+JSON.stringify(req.body));
      console.log('MYDATA CODE '+req.body.code);
-     var request = require('request');
      var options = {
        'method': 'POST',
        'url': 'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.auth.marketingcloudapis.com/v2/token',
